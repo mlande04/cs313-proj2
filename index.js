652992bbed29f2b97249cde0c05a2fa22d854062
Michelle Landen <lan18002@byui.edu>
2019-03-15 12:59:55 -0700

    more files added for chat room

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express = require('express')
+const app = require('express')();
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const io = require('socket.io')(PORT);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+io.on('connection', function(socket) {
+   console.log('a user connected');
+   socket.on('disconnect', function() {
+	   console.log('a user disconnected');
+   });
+});
+
+io.on('connection', function(socket) {
+	socket.on('chat message', function(msg) {
+		io.emit('chat message', msg);
+		console.log('message: ' + msg);
+	});
+});
