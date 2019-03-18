5ae142bc50e6bc533a771f122cff819562dba90b
Michelle Landen <lan18002@byui.edu>
2019-03-18 15:18:21 -0700

    working chat window

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-// This is the main file of our chat app. It initializes a new 
-// express.js instance, requires the config and routes files
-// and listens on a port. Start the application by running
-// 'node app.js' in your terminal
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
-var express = require('express'),
-	app = express();
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
-// This is needed if the app is run on heroku:
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 
-var port = process.env.PORT || 8080;
+// connection and disconnect events
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 
-// Initialize a new socket.io object. It is bound to 
-// the express app, which allows them to coexist.
-
-var io = require('socket.io').listen(app.listen(port));
-
-// Require the configuration and the routes files, and pass
-// the app and io as arguments to the returned functions.
-
-require('./config')(app, io);
-require('./routes')(app, io);
-
-console.log('Your application is running on http://localhost:' + port);
+// chat message events
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
