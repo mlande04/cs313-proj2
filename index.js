4b5c68687acc1668f387631673fefd2c74331d1f
Michelle Landen <lan18002@byui.edu>
2019-03-18 11:41:40 -0700

    added connection code

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const io = require('socket.io')(PORT);
 
+/*
+const http = require('http')(PORT);
+http.createServer(function(req, res){
+	res.writeHead(200, {"Content-type": "text/plain"})
+	res.end("Hello World\n")
+}).listen(PORT)
+*/
+
 //app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -10,6 +18,12 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// Heroku setting for long polling - for using Socket.IO server object
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
 
 io.on('connection', function(socket) {
    console.log('a user connected');
